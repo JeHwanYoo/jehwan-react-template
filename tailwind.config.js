dc9513bbab2ef03b86e59b444fcf8da63207ec84
Jehwan Yoo <yoojehwan1875@gmail.com>
2023-04-06 16:59:02 +0900

    Feat: Add tailwind official plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,10 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
