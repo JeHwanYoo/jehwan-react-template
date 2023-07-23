42c023f9e6c06cad26706a7d68daf9d0b6a762af
Jehwan Yoo <yoojehwan1875@gmail.com>
2023-07-23 12:51:17 +0900

    feat: add jsxSingleQuote option

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
       </header>
     </div>
   )
