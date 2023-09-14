8161b4d1337266b5d6882f1f66cdc1749def7690
Jehwan Yoo <yoojehwan1875@gmail.com>
2023-09-14 13:44:54 +0900

    fix: husky

--- fix-husky.sh
+++ fix-husky.sh
@@ -4,5 +4,5 @@
 # 형식이 깨지기 때문에 이를 고치기 위한 스크립트 입니다.
 
 # shellcheck disable=SC2016
-printf '#!/usr/bin/env sh\n.%s "$(dirname -- "$0")/_/husky.sh"\n\nnpm run lint\nnpm run __test__-no-watch\n' >.husky/pre-commit
+printf '#!/usr/bin/env sh\n.%s "$(dirname -- "$0")/_/husky.sh"\n\nnpm run lint\n' >.husky/pre-commit
 chmod +x .husky/pre-commit
