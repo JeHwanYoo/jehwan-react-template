79521cd066de916dc01e3472cc03109db84dbfcc
Jehwan Yoo <yoojehwan1875@gmail.com>
2023-09-14 15:10:21 +0900

    chore: set prettierc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,8 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended'
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -12,8 +13,9 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true }
+      { allowConstantExport: true },
     ],
-    '@typescript-eslint/no-unused-vars': ['off']
-  }
+    '@typescript-eslint/no-unused-vars': ['off'],
+    'prettier/prettier': 'error',
+  },
 }
--- vitest.config.ts
+++ vitest.config.ts
@@ -14,6 +14,6 @@ export default defineConfig({
     include: ['./__test__/**/*.test.{ts,tsx}'],
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
-    css: true
-  }
+    css: true,
+  },
 })
